c4fc5bc957598444aaf74e9075d9132986a1cedd
chiakiaosima <starsrock5937@gmail.com>
2021-07-24 07:05:17 +0900

    first_commit

--- SampleButton/ViewController.swift
+++ SampleButton/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func dogButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func catButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func frogButton(_ sender: Any) {
+    }
+    
+    
 }
 
